105cdfa97956ed5e33d678961c0471fa6d7e44eb
Noel Vaca <noel.vm.2501@gmail.com>
2022-10-27 19:35:49 -0400

    Documentacion Open API con Swagger

--- demo_nestjs/src/interprete/interprete.controller.ts
+++ demo_nestjs/src/interprete/interprete.controller.ts
@@ -2,32 +2,40 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { InterpreteService } from './interprete.service';
 import { CreateInterpreteDto } from './dto/create-interprete.dto';
 import { UpdateInterpreteDto } from './dto/update-interprete.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { InterpreteEntity } from './entities/interprete.entity';
 
+@ApiTags('interpretes')
 @Controller('interpretes')
 export class InterpreteController {
   constructor(private readonly interpreteService: InterpreteService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: InterpreteEntity })
   create(@Body() createInterpreteDto: CreateInterpreteDto) {
     return this.interpreteService.create(createInterpreteDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: InterpreteEntity, isArray: true })
   findAll() {
     return this.interpreteService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: InterpreteEntity })
   findOne(@Param('id') id: string) {
     return this.interpreteService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: InterpreteEntity })
   update(@Param('id') id: string, @Body() updateInterpreteDto: UpdateInterpreteDto) {
     return this.interpreteService.update(+id, updateInterpreteDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse()
   remove(@Param('id') id: string) {
     return this.interpreteService.remove(+id);
   }
--- demo_nestjs/src/main.ts
+++ demo_nestjs/src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,7 +9,17 @@ async function bootstrap() {
   app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Rest SIS257')
+    .setDescription('API Rest de Desarrollo de Aplicaciones Int/Internet II')
+    .setVersion('0.0.1')
+    .addTag('interpretes, albums, generos, canciones, usuarios, auth')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apidoc', app, document);
+
   await app.listen(process.env.PORT);
-  console.log(`API Rest corriendo en ${await app.getUrl()}`);
+  console.log(`API Rest SIS257 ${await app.getUrl()}/apidoc`);
 }
 bootstrap();
