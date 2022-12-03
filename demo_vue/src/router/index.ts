2214516fe1dc81964b63cd00a59f111a9d3871a5
Noel Vaca <noel.vm.2501@gmail.com>
2022-12-03 10:39:13 -0400

    CRUD en vue de Intérpretes

---
+++ demo_vue/src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/interpretes",
+      name: "interpretes",
+      component: () => import("../views/InterpreteView.vue"),
+      children: [
+      { path: "", component: () => import("../components/interprete/InterpreteList.vue") },
+      { path: "crear", component: () => import("../components/interprete/InterpreteCreate.vue") },
+      { path: "editar/:id", component: () => import("../components/interprete/InterpreteEdit.vue") }
+      ]
+    },    
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
+
+export default router;
